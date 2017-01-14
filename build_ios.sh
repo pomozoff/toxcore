2df87fa7838124a41557c4ffcd00567ccb625281
Anton Pomozov <pomozoff@mail.ru>
2017-01-14 23:04:11 +0300

    mod: Create universal binaries

--- build_ios.sh
+++ build_ios.sh
@@ -18,6 +18,11 @@ OPT_FLAGS="-O3"
 MAKE_JOBS=$(sysctl -n hw.ncpu)
 MIN_IOS_VERSION="9.0"
 
+DEVICE_ARM="device_arm"
+DEVICE_ARM64="device_arm64"
+SIMULATOR_I386="simulator_i386"
+SIMULATOR_X86_64="simulator_x86_64"
+
 dobuild() {
     export CC="$(xcrun -find -sdk ${SDK} cc)"
     export CXX="$(xcrun -find -sdk ${SDK} cxx)"
@@ -45,26 +50,42 @@ SDK="iphoneos"
 ARCH_FLAGS="-arch armv7"
 HOST_FLAGS="${ARCH_FLAGS} -miphoneos-version-min=${MIN_IOS_VERSION} -isysroot $(xcrun -sdk ${SDK} --show-sdk-path)"
 CHOST="arm-apple-darwin"
-PREFIX="device_arm"
+PREFIX="${DEVICE_ARM}"
 dobuild
 
 SDK="iphoneos"
 ARCH_FLAGS="-arch arm64"
 HOST_FLAGS="${ARCH_FLAGS} -miphoneos-version-min=${MIN_IOS_VERSION} -isysroot $(xcrun -sdk ${SDK} --show-sdk-path)"
 CHOST="arm-apple-darwin"
-PREFIX="device_arm64"
+PREFIX="${DEVICE_ARM64}"
 dobuild
 
 SDK="iphonesimulator"
 ARCH_FLAGS="-arch i386"
 HOST_FLAGS="${ARCH_FLAGS} -mios-simulator-version-min=${MIN_IOS_VERSION} -isysroot $(xcrun -sdk ${SDK} --show-sdk-path)"
 CHOST="i386-apple-darwin"
-PREFIX="simulator_i386"
+PREFIX="${SIMULATOR_I386}"
 dobuild
 
 SDK="iphonesimulator"
 ARCH_FLAGS="-arch x86_64"
 HOST_FLAGS="${ARCH_FLAGS} -mios-simulator-version-min=${MIN_IOS_VERSION} -isysroot $(xcrun -sdk ${SDK} --show-sdk-path)"
 CHOST="x86_64-apple-darwin"
-PREFIX="simulator_x86_64"
+PREFIX="${SIMULATOR_X86_64}"
 dobuild
+
+LIB_FILES=( \
+            libtoxav.a \
+            libtoxcore.a \
+            libtoxdns.a \
+            libtoxencryptsave.a \
+          )
+
+for INDEX in "${!LIB_FILES[@]}"; do
+    LIB_FILE="${LIB_FILES[$INDEX]}"
+    lipo -create "${BUILD_PATH}/${DEVICE_ARM}/lib/${LIB_FILE}" \
+                 "${BUILD_PATH}/${DEVICE_ARM64}/lib/${LIB_FILE}" \
+                 "${BUILD_PATH}/${SIMULATOR_I386}/lib/${LIB_FILE}" \
+                 "${BUILD_PATH}/${SIMULATOR_X86_64}/lib/${LIB_FILE}" \
+         -output "${BUILD_PATH}/${LIB_FILE}"
+done
